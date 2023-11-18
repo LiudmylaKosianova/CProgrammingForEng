902974b4b346ee672748a1c92ae3ebae9a5a93d5
snowwhitesheep <snw.wht.lk@gmail.com>
2023-11-18 15:20:08 +0100

    start lab

---
+++ 5_labN4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void){
+
+    double a,b,c,d;
+    a = 1.602e-19; //charge of an electron
+    b = 1.1; //holds current
+    c = 1.2; //electrons per 1 coulomb
+    d = 1.3; //electrons per given current
+
+    printf("Welcome to the Electron Calculator!\n");
+    printf("\n\tEnter your electrical current in Amp:\n");
+    printf("\n\tYou entered something\n");
+
+
+
+
+    return 0;
+}
