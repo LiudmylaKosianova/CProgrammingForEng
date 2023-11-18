9e9756daa56e60fa9548605b0314693c7b880a9c
snowwhitesheep <snw.wht.lk@gmail.com>
2023-11-18 16:03:50 +0100

    continue lab

--- 5_labN4.c
+++ 5_labN4.c
@@ -5,13 +5,17 @@ int main (void){
     double a,b,c,d;
     a = 1.602e-19; //charge of an electron
     b = 1.1; //holds current
-    c = 6.2422e+18; //electrons per 1 coulomb
-    d = 1.3; //electrons per given current
+    c = 1 / a; //electrons per 1 coulomb
+    d = c * b; //electrons per given current
 
     printf("Welcome to the Electron Calculator!\n");
-    printf("\n\tEnter your electrical current in Amp:\n");
-    printf("\n\tYou entered something\n");
+    printf("\n\tEnter your electrical current in Amp: ");
+    scanf("%lf", &b);
+    printf("\n\n");
+    printf("\tYou entered %.4E Amps\n", b);
+    printf("\tCharge of an electron = %.4E\n",a);
     printf("\tThere are %.4E electrons in one Coulomb\n", c);
+    printf("\t%.4E",d);
 
 
 
