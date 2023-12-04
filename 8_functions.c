c042fb5a67f14fadb8ca681539d279512644a859
snowwhitesheep <snw.wht.lk@gmail.com>
2023-12-04 17:34:29 +0100

    functions

--- 8_functions.c
+++ 8_functions.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int f(int x);
+int get_five(void);
 
 int main(){
 
-    int y;
+    int y,yy;
     y = f(2);
+    printf("2 squared is %d\n", y);
+    yy = get_five();
+    printf("I've got %d\n", yy);
     return 0;
 }
 
@@ -13,4 +17,9 @@ int f(int x){
     int y;
     y = x*x;
     return y;
+}
+
+int get_five(void){
+    int get_five = 5;
+    return get_five;
 }
