60d10d0bb6e7d39f15706384ea6d2e465a007a75
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-12 12:47:43 +0100

    arrays

---
+++ 10_arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int A[4];
+
+    A[0] = 22;
+    A[1] = 109;
+    A[2] = 101;
+    A[3] = -8;
+
+    prinft("A[0] = %d \n", A[0]);
+    prinft("A[1] = %d \n", A[1]);
+    prinft("A[2] = %d \n", A[2]);
+    prinft("A[3] = %d \n", A[3]);
+
+    return 0;
+}
