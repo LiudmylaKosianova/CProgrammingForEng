a3f685e0b6c9a2e85d4bb0ad2c89c4cfdcda5834
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-24 12:38:06 +0100

    print 2D array with a nested loop

--- 18_2darray.c
+++ 18_2darray.c
@@ -5,11 +5,11 @@ int main(void){
     double M[3][2];
 
     M[0][0] = 0.0;
-    M[0][1] = 0.0;
-    M[1][0] = 0.0;
-    M[1][1] = 0.0;
-    M[2][0] = 0.0;
-    M[2][1] = 0.0;
+    M[0][1] = 0.1;
+    M[1][0] = 1.0;
+    M[1][1] = 1.1;
+    M[2][0] = 2.0;
+    M[2][1] = 2.1;
 
     /*print array*/
     printf("M[0][0] = %.1lf \n", M[0][0]);
@@ -18,6 +18,16 @@ int main(void){
     printf("M[1][1] = %.1lf \n", M[1][1]);
     printf("M[2][0] = %.1lf \n", M[2][0]);
     printf("M[2][1] = %.1lf \n", M[2][1]);
+
+    /*print 2D array with loop*/
+    printf("Print with a loop \n");
+
+    int i, j;
+    for (i=0; i<3; i++){
+        for(j=0; j<2; j++){
+            printf("M[%d][%d] = %.1lf \n", i, j, M[i][j]);
+        }
+    }
 
     return 0;
 }
