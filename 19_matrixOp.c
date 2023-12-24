c9216a2fb41b3f8bef362e44670e014fdb00b4e8
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-24 13:20:46 +0100

    print addition of matrices

--- 19_matrixOp.c
+++ 19_matrixOp.c
@@ -13,6 +13,31 @@ int main(void){
             C[i][j] = A[i][j] + B[i][j];
         }
     }
+    /*print*/
+
+    printf("\n");
+    for(int i =0; i<2; i++){
+        for(int j =0; j<2; j++){
+            printf("%d\t", A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("    +\n");
+    for(int i =0; i<2; i++){
+        for(int j =0; j<2; j++){
+            printf("%d\t", B[i][j]);
+        }
+        printf("\n");
+    }
+    printf("    =\n");
+    for(int i =0; i<2; i++){
+        for(int j =0; j<2; j++){
+            printf("%d\t", C[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
     return 0;
 }
