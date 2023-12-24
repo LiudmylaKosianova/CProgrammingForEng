b51950dbe71feb7f3f2d20fb8c1e79fb6a9e97f6
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-24 12:31:49 +0100

    print 2d array

--- 18_2darray.c
+++ 18_2darray.c
@@ -13,6 +13,11 @@ int main(void){
 
     /*print array*/
     printf("M[0][0] = %.1lf \n", M[0][0]);
+    printf("M[0][1] = %.1lf \n", M[0][1]);
+    printf("M[1][0] = %.1lf \n", M[1][0]);
+    printf("M[1][1] = %.1lf \n", M[1][1]);
+    printf("M[2][0] = %.1lf \n", M[2][0]);
+    printf("M[2][1] = %.1lf \n", M[2][1]);
 
     return 0;
 }
