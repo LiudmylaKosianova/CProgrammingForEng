e288ceb55687f1c97324a079aac6232137a37421
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-25 11:14:07 +0100

    pointers

---
+++ 20_pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    int n = 109;
+    int *p = &n;
+
+    printf("\n");
+    printf("n = %d \n", n);
+    printf("address of n is %p\n", p);
+
+
+    return 0;
+}
