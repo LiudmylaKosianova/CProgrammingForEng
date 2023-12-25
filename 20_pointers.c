3c91a7c015482b98c5a86c3e4ada13c358157951
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-25 11:24:53 +0100

    more pointers

--- 20_pointers.c
+++ 20_pointers.c
@@ -8,6 +8,21 @@ int main(void){
     printf("\n");
     printf("n = %d \n", n);
     printf("address of n is %p\n", p);
+    printf("address of n is %p\n", &n);
+
+    printf("\tusing n, n = %d", n);
+    printf("\tusing *p, n = %d\n", *p);
+
+    int A_int = 5;
+    int B_int;
+
+    int *A_po = &A_int;
+
+    /*looked at the address, stored in pointer variable *A_po, 
+      and then allocate the value, stored in that address, to the varible B_int*/
+    B_int = *A_po; 
+    
+    printf("A_int is %d, and B_int is %d\n", A_int, B_int);
 
 
     return 0;
