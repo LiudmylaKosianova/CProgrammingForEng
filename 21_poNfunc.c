ee0ced954425f4b3646590ba9b382ac19c69445d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-25 14:15:35 +0100

    fill and print the arrays

--- 21_poNfunc.c
+++ 21_poNfunc.c
@@ -15,6 +15,24 @@ int main(void){
     double Vsin[Npts];
     double *Vsin_p = Vsin;
     double Tstep = (Tmax -Tmin) / (Npts - 1);
+    int i;
+
+    //fill in T array - time
+    T[0] = Tmin;
+    for(i=1; i<Npts; i++){
+        T[i] = T[i-1] + Tstep;
+    }
+
+    //fill Vsin array - sin
+    for(i=0; i<Npts; i++){
+        Vsin[i] = sin(T[i]);
+    }
+
+    //print original arrays
+    for(i=0; i<Npts; i++){
+            printf("T[%d]=%.3lf\tVsin[%d]=%.3lf\n", i, T[i], i, Vsin[i]);
+        }
+
 
     return 0;
 }
