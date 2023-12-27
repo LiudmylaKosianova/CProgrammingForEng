46418bc62a93118cf67340617ba0e858e3f0b338
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-27 11:11:11 +0100

    files

---
+++ 23_files.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define Tmin 0.0
+#define Tmax 6.28
+#define Npts 10
+
+int main(void){
+
+    FILE *fout;
+    fout = fopen("output_file.txt", "w");
+    fprintf(fout, "Hello, file\n");
+    fclose(fout);
+
+    FILE *tout;
+    tout = fopen("tout.txt", "w");
+    double T[Npts];
+    double Tstep = (Tmax - Tmin)/ (Npts -1);
+    int i;
+
+    T[0] = Tmin;
+    for(i =1; i<Npts; i++){
+        T[i] = Tstep + T[i - 1];
+    }
+
+
+
+    return 0;
+}
